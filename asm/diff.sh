31389b4271708415df0f611972431f9069cb23a4
Paul RASTOIN <prastoin@student.42.fr>
2019-04-02 14:11:55 +0200

    cleaning

--- asm/diff.sh
+++
@@ -1,5 +0,0 @@
-./asm test.s
-../ressources/asm test.s
-hexdump yolo.cor > yolo.txt ;
-hexdump test.cor > test.txt ;
-vimdiff yolo.txt test.txt ;
--- asm/lol.c
+++
@@ -1,5 +0,0 @@
-#define STR(z) #z
-
-char *one = STR("lol");
-char *two = STR('c');
-char *tree = STR(licorne magique);
