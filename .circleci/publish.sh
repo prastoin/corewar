a3a594472447985b59b7701f13c7ce4bca88e6f5
Duarte David <deltaduartedavid@gmail.com>
2019-05-05 12:17:17 +0200

    Add .circleci/config.yml

---
+++ .circleci/publish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl
+
+version=$(git rev-parse --short HEAD)
+
+curl -T vm.wasm -udeltaevo:$BINTRAY_KEY https://api.bintray.com/content/deltaevo/corewar/vm/$version/$version/vm.wasm
+curl -X POST -udeltaevo:$BINTRAY_KEY https://api.bintray.com/content/deltaevo/corewar/vm/$version/publish
+sleep 20
+curl -X PUT -H "Content-Type: application/json" -d '{"list_in_downloads": true}' -udeltaevo:$BINTRAY_KEY \
+	https://api.bintray.com/file_metadata/deltaevo/corewar/$version/vm.wasm
