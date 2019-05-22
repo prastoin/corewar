edcd55c3306642e538b20a5c93d63d39e9492f6f
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-05-22 16:58:33 +0200

    Try to set Content-Type

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,7 +3,7 @@
 
 version=$(git rev-parse --short HEAD)
 
-curl -T vm.wasm -udeltaevo:$BINTRAY_KEY https://api.bintray.com/content/deltaevo/corewar/vm/$version/$version/vm.wasm
+curl -T vm.wasm -H "Content-Type: application/wasm" -udeltaevo:$BINTRAY_KEY https://api.bintray.com/content/deltaevo/corewar/vm/$version/$version/vm.wasm
 curl -X POST -udeltaevo:$BINTRAY_KEY https://api.bintray.com/content/deltaevo/corewar/vm/$version/publish
 sleep 20
 curl -X PUT -H "Content-Type: application/json" -d '{"list_in_downloads": true}' -udeltaevo:$BINTRAY_KEY \
